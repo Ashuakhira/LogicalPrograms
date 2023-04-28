3ae723a15a0f59468198d2ee040a5ee597c6dc7e
Ashuakhira <bashok592@gmail.com>
2023-04-28 12:59:03 +0530

    File is added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VowelandConsonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //QuotientAndRemainder.FindQuotientAndRemainder();
+            //FIndLargestNumber.LargestNumber();
+            //VowelAndConsonant.CheckAlphabet();
+              EvenOdd.CheckEvenOdd();
+
+        }
+    }
+}
