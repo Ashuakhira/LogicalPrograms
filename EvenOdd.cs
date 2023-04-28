e11054a9f600a7d8234e61ff515cc95eefd25679
Ashuakhira <bashok592@gmail.com>
2023-04-28 13:03:39 +0530

    File is added

---
+++ EvenOdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelandConsonant
+{
+    public class EvenOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            int i;
+            Console.WriteLine("Please enter the number ");
+            i = int.Parse(Console.ReadLine());
+            if(i % 2 == 0)
+            {
+                Console.WriteLine("Entered Number is Even Number ");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Entered number is Odd NUmber ");
+                    Console.ReadLine();
+            }
+        }
+    }
+}
